48a5ecfbf0b90b5083a51490dea053436abab702
KakhaPh_07 <kk.phutkaradze@gmail.com>
2022-04-23 23:21:38 +0400

    commit last

--- altscript.js
+++ altscript.js
@@ -1,16 +1,22 @@
-var selectedOption = document.getSelection;
-
-function textSpeak() {
-    if(selectedOption){
-        var text = document.getSelection().toString();
-    }
+function speak(){
+    const text = document.getSelection().toString();
     responsiveVoice.speak(text);
 }
 
-// function listen() {
-//     if(selectedOption){
-//         let btn = document.createElement("button");
-//         btn.innerHTML = "Click Me";
-//         document.body.appendChild(btn);
-//     }
-// }
+setInterval(()=>{
+    
+    if(document.getSelection().toString().length > 0){
+        document.getElementById('btnSpeak').style.display = 'block';
+        // get text coords
+        const textCoords = document.getSelection().getRangeAt(0).getBoundingClientRect();
+        const textX = textCoords.x;
+        const textY = textCoords.y;
+        // show btn at text coords
+        document.getElementById('btnSpeak').style.left = textX + 'px';
+        document.getElementById('btnSpeak').style.top = textY + 25 + 'px';
+        
+    }else{
+        document.getElementById('btnSpeak').style.display = 'none';
+    }
+
+}, 800);
--- script.js
+++ script.js
@@ -19,10 +19,26 @@ btnSpeak.addEventListener('click', ()=> {
             toSpeak.voice = voice;
         }
     });
-
     synth.speak(toSpeak);
 });
 
+setInterval(()=>{
+    
+    if(document.getSelection().toString().length > 0){
+        document.getElementById('btnSpeak').style.display = 'block';
+        // get text coords
+        const textCoords = document.getSelection().getRangeAt(0).getBoundingClientRect();
+        const textX = textCoords.x;
+        const textY = textCoords.y;
+        // show btn at text coords
+        document.getElementById('btnSpeak').style.left = textX + 'px';
+        document.getElementById('btnSpeak').style.top = textY + 25 + 'px';
+        
+    }else{
+        document.getElementById('btnSpeak').style.display = 'none';
+    }
+
+}, 800);
 
 function PopulateVoices(){
     voices = synth.getVoices();
